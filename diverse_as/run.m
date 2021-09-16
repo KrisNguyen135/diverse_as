38a31e65944d69eb11120c901f8184b06a33bc53
KrisNguyen135 <nguyenminhquan135@gmail.com>
2021-09-16 14:35:29 -0500

    citeseer for cluster

--- diverse_as/run.m
+++ diverse_as/run.m
@@ -10,7 +10,7 @@ addpath(genpath('../active_search'));
 
 %%% high-level settings
 budget   = 500;
-verbose  = false;
+verbose  = true;
 data_dir = '../data/';
 if ~isdir(data_dir)
     data_dir  = '/storage1/garnett/Active/activelearning/quan/diverse_as/data/';
