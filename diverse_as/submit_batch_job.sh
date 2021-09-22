6e5a168a699e3ae6dbd09d76edb3361f6de90c72
KrisNguyen135 <nguyenminhquan135@gmail.com>
2021-09-22 11:16:19 -0500

    misc

--- diverse_as/submit_batch_job.sh
+++ diverse_as/submit_batch_job.sh
@@ -1,9 +1,8 @@
-export node=mangosteen
-
-for exp in {1..5}
+for exp in {1..20}
 do
   bsub -q "normal" -G SEAS-Lab-Garnett -R "rusage[mem=20]" \
-  -o bjob_output/run.%J -J "run$exp" -m "$node.engr.wustl.edu" \
+  -o bjob_output/run.%J -J "run$exp" \
+  -m "mangosteen.engr.wustl.edu kumquat.engr.wustl.edu soursop.engr.wustl.edu rambutan.engr.wustl.edu" \
   matlab -nodesktop -nosplash -nodisplay -r \
-  "exp = $exp; run; exit;"
+  "exp = $exp; run_square; exit;"
 done
