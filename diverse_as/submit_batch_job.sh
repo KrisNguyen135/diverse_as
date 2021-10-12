daf27cf1607ebbeb83eb85f8bf7147cd9225df32
KrisNguyen135 <nguyenminhquan135@gmail.com>
2021-10-12 09:32:12 -0500

    misc

--- diverse_as/submit_batch_job.sh
+++ diverse_as/submit_batch_job.sh
@@ -1,10 +1,21 @@
-export data=ecfp2
+# export data=ecfp2
+# export group_size=4
+#
+# for exp in {1..5}
+# do
+#   bsub -q "normal" -G SEAS-Lab-Garnett -R "rusage[mem=20]" \
+#   -o bjob_output/run.%J -J "run$exp" -m "kumquat.engr.wustl.edu" \
+#   matlab -nodesktop -nosplash -nodisplay -r \
+#   "exp=$exp; group_size=$group_size; data='$data'; run; exit;"
+# done
+
+export data=ecfp
 export group_size=4
 
-for exp in {1..5}
+for group in {4..10}
 do
   bsub -q "normal" -G SEAS-Lab-Garnett -R "rusage[mem=20]" \
-  -o bjob_output/run.%J -J "run$exp" -m "kumquat.engr.wustl.edu" \
+  -o bjob_output/run.%J -J "run$group" -m "kumquat.engr.wustl.edu" \
   matlab -nodesktop -nosplash -nodisplay -r \
-  "exp=$exp; group_size=$group_size; data='$data'; run; exit;"
+  "group_size=$group_size; data='$data$group'; run; exit;"
 done
