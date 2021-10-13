34b9860474702fd22c8aa8ee7ac942fa167e5b49
KrisNguyen135 <nguyenminhquan135@gmail.com>
2021-10-13 07:30:45 -0500

    misc

--- diverse_as/submit_batch_job.sh
+++ diverse_as/submit_batch_job.sh
@@ -11,11 +11,12 @@
 
 export data=ecfp
 export group_size=4
+export exp=2
 
-for group in {4..10}
+for group in {1..5}
 do
-  bsub -q "normal" -G SEAS-Lab-Garnett -R "rusage[mem=20]" \
-  -o bjob_output/run.%J -J "run$group" -m "kumquat.engr.wustl.edu" \
+  bsub -q "centos8" -G SEAS-Lab-Garnett -R "rusage[mem=20]" \
+  -o bjob_output/run.%J -J "run$group" -R "hname!=node01.engr.wustl.edu" \
   matlab -nodesktop -nosplash -nodisplay -r \
-  "group_size=$group_size; data='$data$group'; run; exit;"
+  "exp=$exp; group_size=$group_size; data='$data$group'; run; exit;"
 done
