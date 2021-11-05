199118ceffc04fdd1cce1086f6fa1d90e22daa10
KrisNguyen135 <nguyenminhquan135@gmail.com>
2021-11-05 11:07:34 -0500

    nn job

---
+++ active_virtual_screening/calculate_nearest_neighbors_all_classes.m
@@ -0,0 +1,57 @@
+num_inactive = 100000;  % number of inactive proteins to randomly subselect
+k            = 500;     % number of nearest neighbors to compute
+
+rng('default');
+
+data_directory        = './../data/drug/processed/';
+precomputed_directory = './../data/drug/precomputed/';
+
+fingerprints = {'ecfp4', 'gpidaph3'};
+
+% choose subset of data to use
+load([data_directory fingerprints{1} '/labels']);
+
+inactive_class = max(labels);
+
+inactive_ind = find(labels == inactive_class);
+active_ind   = find(labels ~= inactive_class);
+
+to_keep = [inactive_ind(randperm(numel(inactive_ind), num_inactive)); ...
+           active_ind];
+
+labels = labels(to_keep);
+labels = labels + 1;
+labels(labels > 120) = 1;
+
+num_proteins = max(labels) - 1;
+
+for fingerprint = fingerprints
+    tic;
+    
+    fprintf('processing fingerprint %s ...\n', fingerprint{:});
+
+    load([data_directory fingerprint{:} '/features']);
+
+    features = sparse(features(:, 2), features(:, 1), 1);
+
+    features = features(:, to_keep);
+    % remove features that are always zero
+    features = features(any(features, 2), :);
+
+    [nearest_neighbors, similarities] = jaccard_nn(features, k);
+
+    filename = sprintf('%s%s_nearest_neighbors_%i.mat', ...
+                       precomputed_directory, ...
+                       fingerprint{:}, ...
+                       num_inactive);
+
+    save(filename, 'nearest_neighbors', 'similarities', 'labels');
+
+    elapsed = toc;
+    if (elapsed < 60)
+      fprintf('done, took %is.\n', ceil(elapsed));
+    else
+      fprintf('done, took %0.1fm.\n', ceil(elapsed / 6) / 10);
+    end
+
+end
--- active_virtual_screening/submit_nearest_neighbors_job.sh
+++ active_virtual_screening/submit_nearest_neighbors_job.sh
@@ -1,12 +1,12 @@
-#BSUB -q normal
+#BSUB -q centos8
 #BSUB -G SEAS-Lab-Garnett
 #BSUB -o bjob_output/nn.%J
 #BSUB -J nn
-#BSUB -m "mangosteen.engr.wustl.edu kumquat.engr.wustl.edu soursop.engr.wustl.edu rambutan.engr.wustl.edu"
+#BSUB -m "node01.engr.wustl.edu"
 #BSUB -R "rusage[mem=120G]"
 
 sleep 5
 ls /storage1/garnett
 sleep 5
 
-matlab -nodesktop -nosplash -nodisplay -r "calculate_nearest_neighbors_multiclass; exit;"
+matlab -nodesktop -nosplash -nodisplay -r "calculate_nearest_neighbors_all_classes; exit;"
