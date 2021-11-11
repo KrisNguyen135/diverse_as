67985a629debdc839a7116e7bb4fce3454ab3cc5
KrisNguyen135 <nguyenminhquan135@gmail.com>
2021-11-11 12:44:43 -0600

    auto

--- diverse_as/submit_drug_job.sh
+++ diverse_as/submit_drug_job.sh
@@ -18,18 +18,7 @@ export exp=1
 
 for group in {38..38}
 do
-  bsub -q "normal" -R "rusage[mem=20]" \
-  -o bjob_output/run.%J -J "run$group" \
-  -m "mangosteen.engr.wustl.edu" \
-  # -m "rambutan.engr.wustl.edu" \
-  # -m "soursop.engr.wustl.edu" \
-  # -m "kumquat.engr.wustl.edu" \
-  # -m "node01.engr.wustl.edu" \
-  # -m "node02.engr.wustl.edu" \
-  # -m "node12.engr.wustl.edu" \
-  # -m "node13.engr.wustl.edu" \
-  # -m "node14.engr.wustl.edu" \
-  # -R "hname!=rambutan.engr.wustl.edu && hname!=soursop.engr.wustl.edu && hname!=kumquat.engr.wustl.edu && hname!=mangosteen.engr.wustl.edu && hname!=node01.engr.wustl.edu  && hname!=node02.engr.wustl.edu  && hname!=node12.engr.wustl.edu  && hname!=node13.engr.wustl.edu  && hname!=node14.engr.wustl.edu" \
+  bsub -q "normal" -R "rusage[mem=20]" -o bjob_output/run.%J -J "run$group" -g /quan/cpu_50 \
   matlab -nodesktop -nosplash -nodisplay -r \
   "exp=$exp; group_size=$group_size; data='$data$group'; run; exit;"
 done
