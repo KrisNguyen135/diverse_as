a92c5e80d61f00bca52961ad6a5c09137a5d6180
KrisNguyen135 <nguyenminhquan135@gmail.com>
2021-11-11 12:15:08 -0600

    auto

--- diverse_as/submit_drug_job.sh
+++ diverse_as/submit_drug_job.sh
@@ -21,6 +21,15 @@ do
   bsub -q "normal" -R "rusage[mem=20]" \
   -o bjob_output/run.%J -J "run$group" \
   -m "mangosteen.engr.wustl.edu" \
+  # -m "rambutan.engr.wustl.edu" \
+  # -m "soursop.engr.wustl.edu" \
+  # -m "kumquat.engr.wustl.edu" \
+  # -m "node01.engr.wustl.edu" \
+  # -m "node02.engr.wustl.edu" \
+  # -m "node12.engr.wustl.edu" \
+  # -m "node13.engr.wustl.edu" \
+  # -m "node14.engr.wustl.edu" \
+  # -R "hname!=rambutan.engr.wustl.edu && hname!=soursop.engr.wustl.edu && hname!=kumquat.engr.wustl.edu && hname!=mangosteen.engr.wustl.edu && hname!=node01.engr.wustl.edu  && hname!=node02.engr.wustl.edu  && hname!=node12.engr.wustl.edu  && hname!=node13.engr.wustl.edu  && hname!=node14.engr.wustl.edu" \
   matlab -nodesktop -nosplash -nodisplay -r \
   "exp=$exp; group_size=$group_size; data='$data$group'; run; exit;"
 done
