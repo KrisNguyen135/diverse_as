d543ac930ac65223a65d95be9586391d1ef2dd07
KrisNguyen135 <nguyenminhquan135@gmail.com>
2021-11-11 10:54:18 -0600

    auto

--- diverse_as/submit_drug_job.sh
+++ diverse_as/submit_drug_job.sh
@@ -10,26 +10,17 @@
 # done
 
 # -R "hname!=rambutan.engr.wustl.edu && hname!=soursop.engr.wustl.edu && hname!=kumquat.engr.wustl.edu && hname!=mangosteen.engr.wustl.edu && hname!=node01.engr.wustl.edu" \
-# -m "mangosteen.engr.wustl.edu"
+# -m "mangosteen.engr.wustl.edu" \
 
 export data=morgan
 export group_size=1
 export exp=1
 
-for group in {16..16}
+for group in {1..5}
 do
   bsub -q "normal" -R "rusage[mem=20]" \
   -o bjob_output/run.%J -J "run$group" \
-  -R "hname!=rambutan.engr.wustl.edu" \
-  -R "hname!=soursop.engr.wustl.edu" \
-  -R "hname!=mangosteen.engr.wustl.edu" \
-  -R "hname!=node02.engr.wustl.edu" \
-  -R "hname!=gnode01.engr.wustl.edu" \
-  -R "hname!=gnode02.engr.wustl.edu" \
-  -R "hname!=gnode05.engr.wustl.edu" \
-  -R "hname!=lotus.engr.wustl.edu" \
-  -R "hname!=node01.engr.wustl.edu" \
-  -R "hname!=node13.engr.wustl.edu" \
+  -m "mangosteen.engr.wustl.edu" \
   matlab -nodesktop -nosplash -nodisplay -r \
   "exp=$exp; group_size=$group_size; data='$data$group'; run; exit;"
 done
