3b4e4252c5845ed1da6ca581b9fe41949e5169c8
KrisNguyen135 <nguyenminhquan135@gmail.com>
2021-11-21 09:39:55 -0600

    auto

--- diverse_as/submit_bmg_job.sh
+++ diverse_as/submit_bmg_job.sh
@@ -3,7 +3,7 @@ export group_size=1
 for exp in {1..1}
 do
   bsub -q "normal" -G SEAS-Lab-Garnett -R "rusage[mem=20]" -g /quan/cpu \
-  -o bjob_output/citeseer.%J -J "c-$group_size-$exp" \
+  -o bjob_output/bmg.%J -J "b-$group_size-$exp" \
   matlab -nodesktop -nosplash -nodisplay -r \
   "exp=$exp; group_size=$group_size; data='bmg'; run; exit;"
 done
