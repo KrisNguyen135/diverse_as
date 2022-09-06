765591ab60eaf67e76459a5bf7ed6e6d9ba71995
KrisNguyen135 <nguyenminhquan135@gmail.com>
2022-09-06 15:35:14 -0500

    add vast job file

---
+++ diverse_as/submit_vast_job.sh
@@ -0,0 +1,7 @@
+for exp in {1..20}
+do
+  bsub -q "normal" -G SEAS-Lab-Garnett -R "rusage[mem=20]" -g /quan/cpu \
+  -o bjob_output/citeseer.%J -J "c-$group_size-$exp" \
+  matlab -nodesktop -nosplash -nodisplay -r \
+  "exp=$exp; data='vast'; run; exit;"
+done
