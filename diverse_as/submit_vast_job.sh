8413ffa216f194a32e79eedff91b973726c3d0c7
KrisNguyen135 <nguyenminhquan135@gmail.com>
2022-09-06 15:37:39 -0500

    modify vast job file

--- diverse_as/submit_vast_job.sh
+++ diverse_as/submit_vast_job.sh
@@ -1,7 +1,8 @@
 for exp in {1..20}
 do
   bsub -q "normal" -G SEAS-Lab-Garnett -R "rusage[mem=20]" -g /quan/cpu \
-  -o bjob_output/citeseer.%J -J "c-$group_size-$exp" \
+  -m "mangosteen.engr.wustl.edu kumquat.engr.wustl.edu soursop.engr.wustl.edu rambutan.engr.wustl.edu" \
+  -o bjob_output/vast.%J -J "c-$group_size-$exp" \
   matlab -nodesktop -nosplash -nodisplay -r \
   "exp=$exp; data='vast'; run; exit;"
 done
