8fdcbd2ef28fc7ccaacbd7e39cbf7a5988562d08
KrisNguyen135 <nguyenminhquan135@gmail.com>
2022-09-13 08:10:53 -0500

    add drug cont run job

--- diverse_as/continue_run.m
+++ diverse_as/continue_run.m
@@ -1,17 +1,16 @@
-train_ind  = '../data/misc/train_ind';
-group_size = 10;
-data       = 'morgan1';
-
-assert(isfile(train_ind), sprintf('%s is not a file', train_ind));
-
 if ~exist('exp',        'var'), exp        = 1; end
-if ~exist('group_size', 'var'), group_size = 1; end
-if ~exist('data',       'var'), data       = 'morgan1'; end
+if ~exist('group_size', 'var'), group_size = 4; end
+if ~exist('data',       'var'), data       = 'citeseer'; end
 if ~exist('utility',    'var'), utility    = 'log'; end
 % if ~exist('policy',     'var'), policy     = 'classical ens'; end
-if ~exist('policy',     'var'), policy     = 'ens jensen greedy'; end
+if ~exist('policy',     'var'), policy     = 'greedy'; end
 % if ~exist('policy',     'var'), policy     = 'greedy'; end
 % if ~exist('policy',     'var'), policy     = 'round robin greedy'; end
+% if ~exist('policy',     'var'), policy     = 'round robin ucb'; end
+% if ~exist('policy',     'var'), policy     = 'round robin ens'; end
+% if ~exist('policy',     'var'), policy     = 'malkomes'; end
+% if ~exist('policy',     'var'), policy     = 'he-carbonell'; end
+% if ~exist('policy',     'var'), policy     = 'van'; end
 
 addpath(genpath('../'));
 addpath(genpath('../active_learning'));
@@ -23,23 +22,32 @@ group_size
 data
 policy
 
-budget   = 500
-verbose  = true;
-data_dir = '../data/';
+budget     = 500
+% result_dir = sprintf('results_%d', budget);
+result_dir = 'results';
+verbose    = true;
+data_dir   = '../data/';
 if ~isdir(data_dir)
     data_dir  = '/storage1/garnett/Active/activelearning/quan/diverse_as/data/';
 end
 
 [problem, labels, weights, alpha, nns, sims] = load_data(data, data_dir, exp, group_size);
+% result_dir = 'results_0002';
 rng(exp);
 if contains(data, 'gpidaph')
     rng(exp + 1);  % to get different init data than ecfp experiments
 end
 
-% read in partial results
-train_ind    = load(train_ind);
+train_ind = load('train_ind.csv');
+budget    = budget - numel(train_ind) + 1;
+
+% randomly select a positive for each class
+% train_ind = [];
+% for i = 2:problem.num_classes
+%     train_ind = [train_ind; randsample(find(labels == i), 1)];
+% end
+
 train_labels = labels(train_ind);
-budget       = budget - numel(train_ind) + 1;
 
 %%% experiment details
 problem.verbose     = verbose;
@@ -57,6 +65,8 @@ selector     = get_selector(@unlabeled_selector);
 switch utility
 case 'log'
     utility_function = @log_utility;
+case 'sqrt'
+    utility_function = @sqrt_utility;
 case 'threshold'
     threshold        = 2;
     utility_function = get_utility_function(@threshold_utility, threshold);
@@ -70,18 +80,34 @@ case 'greedy'
 case 'round robin greedy'
     policy = get_policy(@round_robin_greedy, model);
 case 'round robin ucb'
-    beta = 0.1;
+    beta = 10.0;
+
+    name       = sprintf('%s_%.1f', name, beta);
+    result_dir = 'results_ucb';
+
     policy = get_policy(@round_robin_ucb, model, beta);
+case 'round robin ens'
+    compute_limit = 500;
+    sample_limit  = 500;
+    name          = sprintf('%s_%d', name, compute_limit);
+    result_dir    = 'results_rr_ens';
+
+    policy = get_policy(@round_robin_ens, model, model_update, [], false, ...
+                        compute_limit, sample_limit);
 case 'classical greedy'
     policy = get_policy(@classical_greedy, model);
 case 'classical ens'
     compute_limit = 500;
     sample_limit  = 500;
+    name          = sprintf('%s_%d', name, compute_limit);
+
     policy = get_policy(@classical_ens, model, model_update, [], false, ...
                         compute_limit, sample_limit);
 case 'ens jensen greedy'
     compute_limit = 500;
     sample_limit  = 500;
+    name          = sprintf('%s_%d', name, compute_limit);
+
     batch_utility_function = get_batch_utility_function(@jensen, model);
 
     if group_size == 1
@@ -96,6 +122,21 @@ case 'ens jensen greedy'
         @jensen_upperbound, weights, nns', sims');
     policy = get_policy(@ens_base, model, batch_policy, batch_utility_function, ...
         utility_upperbound_function, true, compute_limit, sample_limit);
+case 'malkomes'
+    result_dir    = 'results_malkomes';
+    sim_threshold = 0.5;
+    name          = sprintf('%s_%.2f', name, sim_threshold);
+    policy        = get_policy(@malkomes, model, nns', sims', sim_threshold);
+case 'he-carbonell'
+    result_dir = 'results_he_carbonell';
+    name       = 'he_cabornell';
+    policy     = get_he_carbonell_policy(nns', sims');
+case 'van'
+    result_dir     = 'results_van';
+    tradeoff_param = 0.75;
+    beta           = 10;
+    name           = sprintf('%s_%.2f_%.2f', name, tradeoff_param, beta);
+    policy         = get_policy(@van, model, tradeoff_param, beta);
 end
 
 if problem.verbose
@@ -103,6 +144,7 @@ if problem.verbose
     disp(train_labels);
     fprintf('utility function: %s\n', problem.utility);
     fprintf('policy: %s\n', name);
+    fprintf('result dir: %s\n', result_dir);
 end
 
 %%% run experiment
@@ -112,8 +154,7 @@ message_prefix = sprintf('Exp %d: ', exp);
     problem, train_ind, train_labels, labels, selector, utility_function, policy, ...
     message_prefix);
 
-% result_dir = './';
-% result_dir = fullfile(result_dir, 'results_cont', data, int2str(group_size), name);
+% result_dir = fullfile('./', result_dir, data, int2str(group_size), name)
 % if ~isdir(result_dir), mkdir(result_dir); end
 %
 % writematrix(train_ind, ...
---
+++ diverse_as/submit_cont_drug_job.sh
@@ -0,0 +1,11 @@
+export data=morgan
+export group_size=14
+export exp=1
+
+for group in 1
+do
+  bsub -q "normal" -G SEAS-Lab-Garnett -R "rusage[mem=20]" -o bjob_output/cont_drug.%J -J "d-$group_size-$group" -g /quan/cpu \
+  -m "mangosteen.engr.wustl.edu kumquat.engr.wustl.edu soursop.engr.wustl.edu rambutan.engr.wustl.edu" \
+  matlab -nodesktop -nosplash -nodisplay -r \
+  "exp=$exp; group_size=$group_size; data='$data$group'; continue_run; exit;"
+done
